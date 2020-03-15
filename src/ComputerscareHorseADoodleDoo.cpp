fd4cd20d598e7e4e828651054265d8ec03f19d44
Adam M <aemalone@gmail.com>
2020-03-14 21:27:26 -0500

    Horse a doodle doo makes good patt pat

--- src/ComputerscareHorseADoodleDoo.cpp
+++ src/ComputerscareHorseADoodleDoo.cpp
@@ -5,33 +5,46 @@
 struct ComputerscareHorseADoodleDoo;
 
 struct HorseSequencer {
-	float pattern=0.f;
-	int numSteps=8;
-	int currentStep=-1;
-	float density=0.f;
-	int primes[16] = {30011,36877,26627,32833,66797,95153,66553,84857,32377,79589,25609,20113,70991,86533,21499,32491};
-	int otherPrimes[16] = {80651,85237,11813,22343,19543,28027,9203,39521,42853,58411,33811,76771,10939,22721,17851,10163};
-	std::vector<std::vector<int>> octets={{0,0,0,0},{0,0,0,1},{0,0,1,0},{0,0,1,1},{0,1,0,0},{0,1,0,1},{0,1,1,0},{0,1,1,1},{1,1,0,0},{1,1,0,1},{1,1,1,0},{1,1,1,1}};
+	float pattern = 0.f;
+	int numSteps = 8;
+	int currentStep = -1;
+	float density = 0.f;
+	int primes[16] = {30011, 36877, 26627, 32833, 66797, 95153, 66553, 84857, 32377, 79589, 25609, 20113, 70991, 86533, 21499, 32491};
+	int otherPrimes[16] = {80651, 85237, 11813, 22343, 19543, 28027, 9203, 39521, 42853, 58411, 33811, 76771, 10939, 22721, 17851, 10163};
+
+	std::vector<std::vector<int>> octets = {{0, 0, 0, 0}, {0, 0, 0, 1}, {0, 0, 1, 0}, {0, 0, 1, 1}, {0, 1, 0, 0}, {0, 1, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 1}, {1,0,0,0},{1,0,0,1},{1,0,1,0},{1,0,1,1},{1, 1, 0, 0}, {1, 1, 0, 1}, {1, 1, 1, 0}, {1, 1, 1, 1}};
+	std::vector<int> somethin={1,0,0,1};
 	std::vector<int> absoluteSequence;
-	HorseSequencer(float patt,int steps,float dens) {
-		numSteps=steps;
-		density=dens;
-		pattern=patt;
+	HorseSequencer(float patt, int steps, float dens) {
+		numSteps = steps;
+		density = dens;
+		pattern = patt;
 		makeAbsolute();
 	}
 	void makeAbsolute() {
-		absoluteSequence.resize(0);
-		for(int i = 0; i < 16; i++) {
-			int dex = ((int)std::floor(pattern*primes[i])+otherPrimes[i])%16;
-			absoluteSequence.push_back(dex < 8 ? 0 : 1);
+		std::vector<int> newSeq;
+		std::vector<int> thisOct;
+		newSeq.push_back(1);
+		newSeq.resize(0);
+		DEBUG("valuu:%f",pattern);
+		for (int i = 0; i < 16; i++) {
+			int dex = ((int)std::floor(pattern * primes[i]) + otherPrimes[i]) % 16;
+			//DEBUG("i:dex:%i",dex);
+
+			thisOct=octets[dex];
+			//vector1.insert( vector1.end(), vector2.begin(), vector2.end() );
+			newSeq.insert(newSeq.end(),thisOct.begin(),thisOct.end());
+			//absoluteSequence.push_back(dex < 8 ? 0 : 1);
 		}
+		printVector(newSeq);
+		absoluteSequence = newSeq;
 	}
 	void tick() {
 		currentStep++;
 		currentStep %= numSteps;
 	}
 	void reset() {
-		currentStep=0;
+		currentStep = 0;
 	}
 	int get() {
 		return absoluteSequence[currentStep];
@@ -44,9 +57,10 @@ struct HorseSequencer {
 
 struct ComputerscareHorseADoodleDoo : Module {
 	int counter = 0;
-	int numChannels=1;
+	int numChannels = 1;
 	ComputerscareSVGPanel* panelRef;
-	float currentValues[16]={0.f};
+	float currentValues[16] = {0.f};
+	bool atFirstStepPoly[16] = {false};
 	enum ParamIds {
 		PATTERN_KNOB,
 		STEPS_KNOB,
@@ -66,7 +80,7 @@ struct ComputerscareHorseADoodleDoo : Module {
 		TRIGGER_OUTPUT,
 		EOC_OUTPUT,
 		REST_OUTPUT,
-		NUM_OUTPUTS 
+		NUM_OUTPUTS
 	};
 	enum LightIds {
 		NUM_LIGHTS
@@ -74,68 +88,87 @@ struct ComputerscareHorseADoodleDoo : Module {
 
 
 	rack::dsp::SchmittTrigger clockInputTrigger;
-  	rack::dsp::SchmittTrigger resetInputTrigger;
+	rack::dsp::SchmittTrigger resetInputTrigger;
 
-  	rack::dsp::SchmittTrigger clockManualTrigger;
-  	rack::dsp::SchmittTrigger resetManualTrigger;
+	rack::dsp::SchmittTrigger clockManualTrigger;
+	rack::dsp::SchmittTrigger resetManualTrigger;
 
-  	float lastPatternKnob=0.f;
-  	float lastStepsKnob=0.f;
-  	float lastDensityKnob=0.f;
+	float lastPatternKnob = 0.f;
+	float lastStepsKnob = 0.f;
+	float lastDensityKnob = 0.f;
 
-  	int seqVal= 0.f;
+	int seqVal = 0.f;
 
-  	HorseSequencer seq = HorseSequencer(0.f,8,0.f);
+	HorseSequencer seq = HorseSequencer(0.f, 8, 0.f);
 
 	ComputerscareHorseADoodleDoo()  {
 
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 
-		configParam(PATTERN_KNOB,0.f,10.f,0.f,"Pattern");
-		configParam(STEPS_KNOB,2.f,64.f,8.f,"Number of Steps");
-		configParam(DENSITY_KNOB,-2.f,2.f,0.f,"Density");
-		
-		seq = HorseSequencer(0.f,8,0.f);
+		configParam(PATTERN_KNOB, 0.f, 10.f, 0.f, "Pattern");
+		configParam(STEPS_KNOB, 2.f, 64.f, 8.f, "Number of Steps");
+		configParam(DENSITY_KNOB, -2.f, 2.f, 0.f, "Density");
+
+		seq = HorseSequencer(0.f, 8, 0.f);
 
 
 	}
-	void patternKnobChanged(float pattern,int numSteps,float density) {
-		seq=HorseSequencer(pattern,numSteps,density);
+	void patternKnobChanged(float pattern, int numSteps, float density) {
+		seq = HorseSequencer(pattern, numSteps, density);
 	}
-
+	void checkKnobChanges() {
+		float patternKnob = params[PATTERN_KNOB].getValue();
+			float stepsKnob = params[STEPS_KNOB].getValue();
+			float densityKnob = params[DENSITY_KNOB].getValue();
+			if (patternKnob != lastPatternKnob || stepsKnob != lastStepsKnob || densityKnob != lastDensityKnob) {
+				patternKnobChanged(patternKnob, stepsKnob, densityKnob);
+			}
+			lastPatternKnob = patternKnob;
+			lastStepsKnob = stepsKnob;
+			lastDensityKnob = densityKnob;
+	}
 	void process(const ProcessArgs &args) override {
 		counter++;
-		if(counter > 8) {
+		if (counter > 1081) {
 			counter = 0;
-			float patternKnob=params[PATTERN_KNOB].getValue();
-			float stepsKnob=params[STEPS_KNOB].getValue();
-			float densityKnob=params[DENSITY_KNOB].getValue();
-			if(patternKnob != lastPatternKnob || stepsKnob != lastStepsKnob || densityKnob != lastDensityKnob) {
-				patternKnobChanged(patternKnob,stepsKnob,densityKnob);
-			}
-			lastPatternKnob=patternKnob;
-			lastStepsKnob=stepsKnob;
-			lastDensityKnob=densityKnob;
 		}
 
 		bool clockInputHigh = clockInputTrigger.isHigh();
 		bool clocked = clockInputTrigger.process(inputs[CLOCK_INPUT].getVoltage());
 
-		if(clocked) {
+		if (clocked) {
 			seqVal = seq.tickAndGet();
+			for (int ch = 0; ch < numChannels; ch++) {
+				atFirstStepPoly[ch] =  (seq.currentStep == 0);
+			}
+			checkKnobChanges();
 		}
 		if (inputs[CLOCK_INPUT].isConnected()) {
-	      for (int ch = 0; ch < numChannels; ch++) {
-	        outputs[TRIGGER_OUTPUT].setVoltage((clockInputHigh && seqVal==1/*activePolyStep[i][ch]*/) ? 10.0f : 0.0f, ch);
-	        //outputs[EOC_OUTPUT].setVoltage((currentTriggerIsHigh && atFirstStepPoly[ch]) ? 10.f : 0.0f, ch);
-	      }
-	    }
-	    else {
-	     /* for (int ch = 0; ch < numChannels; ch++) {
-	        outputs[TRG_OUTPUT + i].setVoltage((globalGateIn && activePolyStep[i][ch]) ? 10.0f : 0.0f, ch);
-	        outputs[FIRST_STEP_OUTPUT + i].setVoltage((globalGateIn && atFirstStepPoly[ch]) ? 10.f : 0.0f, ch);
-	      }*/
-	    }
+			for (int ch = 0; ch < numChannels; ch++) {
+				outputs[TRIGGER_OUTPUT].setVoltage((clockInputHigh && seqVal == 1/*activePolyStep[i][ch]*/) ? 10.0f : 0.0f, ch);
+				//outputs[EOC_OUTPUT].setVoltage((currentTriggerIsHigh && atFirstStepPoly[ch]) ? 10.f : 0.0f, ch);
+			}
+		}
+		else {
+			/* for (int ch = 0; ch < numChannels; ch++) {
+			   outputs[TRG_OUTPUT + i].setVoltage((globalGateIn && activePolyStep[i][ch]) ? 10.0f : 0.0f, ch);
+			   outputs[FIRST_STEP_OUTPUT + i].setVoltage((globalGateIn && atFirstStepPoly[ch]) ? 10.f : 0.0f, ch);
+			 }*/
+		}
+
+
+		//if (outputs[EOC_OUTPUT].isConnected()) {
+			for (int ch = 0; ch < numChannels; ch++) {
+				
+					if(atFirstStepPoly[ch]) {
+						
+							outputs[EOC_OUTPUT].setVoltage((clockInputHigh && atFirstStepPoly[ch]) ? 10.f : 0.0f, ch);
+						
+						}
+				}
+				//outputs[EOC_OUTPUT].setVoltage((currentTriggerIsHigh && atFirstStepPoly[ch]) ? 10.f : 0.0f, ch);
+			//}	
+		
 
 
 	}
@@ -163,13 +196,13 @@ struct ComputerscareHorseADoodleDooWidget : ModuleWidget {
 //		    ParamWidget* stepsKnob =  createParam<LrgKnob>(Vec(108, 30), module, ComputerscarePatchSequencer::STEPS_PARAM);
 
 
-			addInput(createInput<InPort>(Vec(18, 184), module, ComputerscareHorseADoodleDoo::CLOCK_INPUT));
-			addInput(createInput<InPort>(Vec(18, 204), module, ComputerscareHorseADoodleDoo::RESET_INPUT));
+		addInput(createInput<InPort>(Vec(18, 184), module, ComputerscareHorseADoodleDoo::CLOCK_INPUT));
+		addInput(createInput<InPort>(Vec(18, 204), module, ComputerscareHorseADoodleDoo::RESET_INPUT));
 
 
-		addLabeledKnob("Pattern",5,90,module,0,-2,0);
-		addLabeledKnob("Num Steps",5,140,module,1,0,0);
-		addLabeledKnob("Density",10,250,module,2,0,0);
+		addLabeledKnob("Pattern", 5, 90, module, 0, -2, 0);
+		addLabeledKnob("Num Steps", 5, 140, module, 1, 0, 0);
+		addLabeledKnob("Density", 10, 250, module, 2, 0, 0);
 
 		addOutput(createOutput<PointingUpPentagonPort>(Vec(38, 194), module, ComputerscareHorseADoodleDoo::TRIGGER_OUTPUT));
 		addOutput(createOutput<PointingUpPentagonPort>(Vec(38, 224), module, ComputerscareHorseADoodleDoo::EOC_OUTPUT));
