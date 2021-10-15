0b54a15dd5f3cb09067dea5cdb40937a1c2e0c71
Adam M <aemalone@gmail.com>
2021-10-15 11:47:32 -0500

    comment out unused (?) MenuParams part that doesnt like v2

--- src/MenuParams.hpp
+++ src/MenuParams.hpp
@@ -107,14 +107,14 @@ struct MenuParam : MenuEntry {
 			//addChild(johnLabel);
 		}
 		else if (type == 1) {
-			pWidget = new MediumDotSnapKnob();
+			/*pWidget = new MediumDotSnapKnob();
 			pWidget->paramQuantity = param;
 			pWidget->box.pos = Vec(controlRightMargin, 0);
 
 			addChild(pWidget);
 			johnLabel = construct<MenuLabel>(&MenuLabel::text, param->getLabel());
 			johnLabel->box.pos = Vec((type == 2 ? slider->box.size.x : pWidget->box.size.x) + controlRightMargin * 2, 0);
-			addChild(johnLabel);
+			addChild(johnLabel);*/
 		}
 		else if (type == 2) {
 			slider = new SmoothSlider(param);
