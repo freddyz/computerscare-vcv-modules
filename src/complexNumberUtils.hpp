734ed01108b4e16e2753732187930642c9f76c75
AdamPorcineFudgepuppy <>
2024-06-19 10:42:05 -0500

    add complex num utils header

---
+++ src/complexNumberUtils.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+// R^N->C^N
+// create
+
+
+// R->C^N
+
+
+// C^N -> R^N
+// Re(z), Im(z), |z|, Arg(z)
+
+
+
+// C^N -> C^N
+// add, subtract, divide, multiply
+// 1-z
+// 1/z
+// conjugate z*
+// z^p
+// (1-z)^p
+
+
+// C^N -> C
+// sum
+// average
+// product
+
+// C^N -> R
+// sum of square modulus
